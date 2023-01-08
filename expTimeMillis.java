206c1d068ea33e90be4cc1fb455157cc89fef31e
Ravi Ranjan <raviranjan1600@gmail.com>
2023-01-08 09:59:19 +0530

    Update expTimeMillis.java

--- expTimeMillis.java
+++ expTimeMillis.java
@@ -1,3 +1,12 @@
-  Date expiration = new Date();
+                public S3PayloadStorage(IDGenerator idGenerator, S3Properties properties) {
+                  this.idGenerator = idGenerator;
+                  bucketName = properties.getBucketName();
+                  expirationSec = properties.getSignedUrlExpirationDuration().getSeconds();
+                  String region = properties.getRegion();
+                  s3Client = AmazonS3ClientBuilder.standard().withRegion(region).build();
+            }
+
+
+            Date expiration = new Date();
             long expTimeMillis = expiration.getTime() + 1000 * expirationSec;
             expiration.setTime(expTimeMillis);
